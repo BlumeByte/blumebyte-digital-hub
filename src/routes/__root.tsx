94638d39bc4315d6a5a607603f97027598241839
Dzornoboa <37811509+dzornoboa@users.noreply.github.com>
2026-09-02 13:03:34 +0000

    Mount cookie consent globally

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Header } from "@/components/site/Header";
 import { Footer } from "@/components/site/Footer";
 import { WhatsAppButton } from "@/components/site/WhatsAppButton";
+import { CookieConsent } from "@/components/site/CookieConsent";
 import { Toaster } from "@/components/ui/sonner";
 
 function NotFoundComponent() {
@@ -145,12 +146,12 @@ function RootComponent() {
       <div className="flex min-h-screen flex-col">
         <Header />
         <main className="flex-1">
-          {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
           <Outlet />
         </main>
         <Footer />
       </div>
       <WhatsAppButton />
+      <CookieConsent />
       <Toaster />
     </QueryClientProvider>
   );
